9372c63d0b0eb3fb788b64b5ff9cdd613f9931ca
Anirban Banerjee <anirbanbanerjee@Anirbans-Mac-mini.local>
2026-01-18 12:50:12 +0530

    k8s version upgraded

--- eks/main.tf
+++ eks/main.tf
@@ -42,10 +42,10 @@ module "eks" {
   eks_managed_node_groups = {
     nodes = {
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      max_size     = 1
+      desired_size = 1
 
-      instance_type = ["t2.small"]
+      instance_types = ["t3.medium"]
     }
   }
 
